2a8d55909fb46c5cdc739345c8ce76d315965349
Liloudini Aziz <zizoa1er@gmail.com>
2017-05-12 17:32:15 +0200

    Mise en place du projet vide

---
+++ src/main/java/fr/liloudini/EcoleSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package fr.liloudini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoleSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoleSpringMvcApplication.class, args);
+	}
+}
