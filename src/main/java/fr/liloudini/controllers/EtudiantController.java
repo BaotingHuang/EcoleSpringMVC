c2dcb5146765f91171b4311819ed4b5b2b6574c7
Liloudini Aziz <zizoa1er@gmail.com>
2017-05-13 15:10:31 +0200

    Page liste des étudiants

--- src/main/java/fr/liloudini/controllers/EtudiantController.java
+++ src/main/java/fr/liloudini/controllers/EtudiantController.java
@@ -3,9 +3,12 @@ package fr.liloudini.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.liloudini.dao.EtudiantRepository;
 import fr.liloudini.entities.Etudiant;
@@ -18,9 +21,18 @@ public class EtudiantController {
 	private EtudiantRepository etr;
 	
 	@RequestMapping(value="/etudiants")
-	public String index(Model model){
-		List<Etudiant> etudiants = etr.findAll();
-		System.out.println(etudiants.size());
+	public String index(Model model,
+						@RequestParam(name="pc",defaultValue="0")Integer pageCourante){
+		
+		Page<Etudiant> etudiants = etr.findAll(new PageRequest(pageCourante,5));
+		System.out.println();
+		
+		Integer[] pages = new Integer[etudiants.getTotalPages()];
+		for (int i = 0; i < pages.length; i++) {pages[i]=i;}
+		model.addAttribute("nombrePage", pages.length);
+		model.addAttribute("pages", pages);
+		model.addAttribute("pageLast",etudiants.getTotalPages());
+		model.addAttribute("pageCourante",pageCourante);
 		model.addAttribute("etudiants", etudiants);
 		return "etudiants";
 	}
